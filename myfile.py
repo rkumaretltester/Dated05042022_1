6bb5407901139f0bcb236a7df1b52649928c83f9
Ranjith Kumar <rkumar.etltester@gmail.com>
2022-04-11 19:03:01 +0530

    myfile.py Leap Year Code added with prime

--- myfile.py
+++ myfile.py
@@ -18,4 +18,30 @@ elif (year % 4 ==0) and (year % 100 != 0):
 # if not divided by both 400 (century year) and 4 (not century year)
 # year is not leap year
 else:
-    print("{0} is not a leap year".format(year))
+    print("{0} is not a leap year".format(year)
+
+# Program to check if a number is prime or not
+
+num = 29
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+# define a flag variable
+flag = False
+
+# prime numbers are greater than 1
+if num > 1:
+    # check for factors
+    for i in range(2, num):
+        if (num % i) == 0:
+            # if factor is found, set flag to True
+            flag = True
+            # break out of loop
+            break
+
+# check if flag is True
+if flag:
+    print(num, "is not a prime number")
+else:
+    print(num, "is a prime number"))
